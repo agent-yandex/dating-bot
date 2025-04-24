6f457650ff3c8e2cd824cdce9053e8f0a9350c51
TeleginSergey <telegins125@gmail.com>
2025-04-24 13:27:38 +0300

    add tg bot token

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/puddle/v2 v2.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,21 +1,36 @@
 package config
 
-import "os"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type AppConfig struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+	DBHost             string
+	DBPort             string
+	DBUser             string
+	DBPassword         string
+	DBName             string
+	TELEGRAM_BOT_TOKEN string
 }
 
 func LoadConfig() AppConfig {
+	err := godotenv.Load("./config/.env")
+	if err != nil {
+		// TODO
+		// Если файл не найден, можно продолжить с переменными окружения из системы
+		// или завершить с ошибкой, в зависимости от ваших требований
+		// Здесь оставим как опциональное логирование в main
+		return AppConfig{}
+	}
+
 	return AppConfig{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
+		DBHost:             os.Getenv("DB_HOST"),
+		DBPort:             os.Getenv("DB_PORT"),
+		DBUser:             os.Getenv("DB_USER"),
+		DBPassword:         os.Getenv("DB_PASSWORD"),
+		DBName:             os.Getenv("DB_NAME"),
+		TELEGRAM_BOT_TOKEN: os.Getenv("TELEGRAM_BOT_TOKEN"),
 	}
 }
